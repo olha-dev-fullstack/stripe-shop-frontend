75877ab6a6f5fc91b09bd0359f2c07d1bfd94622
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-05-07 14:02:52 +0300

    add frontend code

---
+++ src/firebase/index.js
@@ -0,0 +1,48 @@
+import firebase from "firebase/app";
+import "firebase/firestore"; // for the db
+import "firebase/auth";
+console.log('12312313', process.env.REACT_APP_API_KEY);
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+firebase.initializeApp(config);
+
+const firestore = firebase.firestore();
+const auth = firebase.auth();
+
+const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) {
+    return;
+  }
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`); //users/uniq26535
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export { firestore, createUserProfileDocument, auth };
